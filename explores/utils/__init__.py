830996671c69f7d47d9368343d9e95fbb10d4145
Surasee <bightsurasee@gmail.com>
2021-09-23 15:45:41 +0700

    add utils.utils to init

--- explores/utils/__init__.py
+++ explores/utils/__init__.py
@@ -1,5 +1,6 @@
 from utils.cluster import GPS_DBScan, GPS_Kmean, GPS_MeanShift
 from utils.stop_detector import StopDetector
-from utils.stop_detector import FindTravelTime
+from utils.find_travel_time import FindTravelTime
 from utils.find_intersect import FindIntersect
+from utils.utils import load_gps_data, get_road_data  
 
