1732b8a238faeec3ded8fa740f3547bca1769e02
Nachaphat <mr.ntagfb@gmail.com>
2021-10-07 21:08:37 +0700

    [+] add train pipeline and early stopping

--- models/lstm/model/lstm.py
+++ models/lstm/model/lstm.py
@@ -1,19 +1,34 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class LSTM(nn.Module):
+class LSTM_Model(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size, dropout=0.2):
-        super(LSTM, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        super(LSTM_Model, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
         self.dropout = nn.Dropout(dropout)
-        self.linear = nn.Linear(num_layers * hidden_size, output_size)
+        self.linear2 = nn.Linear(num_layers * hidden_size, output_size)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for name, param in self.lstm.named_parameters():
+            if 'bias' in name:
+                 nn.init.constant_(param, 0.0)
+            elif 'weight_ih' in name:
+                 nn.init.kaiming_normal_(param)
+            elif 'weight_hh' in name:
+                 nn.init.orthogonal_(param)
 
     def forward(self, x):
         batch_size = x.shape[0]
 
+        x = F.relu(self.linear1(x))
+
         lstm_out, (h_n, _) = self.lstm(x)
         out = h_n.permute(1, 0, 2).reshape(batch_size, -1)
         out = self.dropout(out)
-        out = self.linear(out)
+        out = self.linear2(out)
         
         return out[:, -1]
--- models/lstm/model/optimizer.py
+++ models/lstm/model/optimizer.py
@@ -1,11 +1,11 @@
 import torch.optim as optim
 
 def config_optimizer(cfg, param):
-    print(f"using {cfg['optimizer']['name']}: learning rate = {cfg['optimizer']['lr']}, momentum = {cfg['optimizer']['momentum']}, weight_decay = {cfg['optimizer']['weight_decay']}")
+    print(f"Using [{cfg['optimizer']['name']}]: learning rate = {cfg['optimizer']['lr']}, weight_decay = {cfg['optimizer']['weight_decay']}")
     
-    if cfg['optimizer']['name'] == 'sgd':
+    if cfg['optimizer']['name'] is 'sgd':
         optimizer = optim.SGD(param, lr=cfg['optimizer']['lr'], momentum=cfg['optimizer']['momentum'], weight_decay=cfg['optimizer']['weight_decay'])
-    elif cfg['optimizer']['name'] == 'adam':
+    elif cfg['optimizer']['name'] is 'adam':
         optimizer = optim.Adam(param, lr=cfg['optimizer']['lr'], betas=cfg['optimizer']['beta'], eps=cfg['optimizer']['eps'], weight_decay=cfg['optimizer']['weight_decay'])
     else:
         AssertionError('optimizer can not be recognized.')
