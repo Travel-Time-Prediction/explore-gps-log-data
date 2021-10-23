299ed4232544cf78d5034b63111e48e4a99eb250
Surasee <brightsurasee@gmail.com>
2021-10-23 17:42:53 +0700

    [~] Rewite ConvLSTM

---
+++ models/conv_lstm/dataset/__init__.py
@@ -0,0 +1,5 @@
+from model.convlstm import TravelTimeModel
+from model.early_stopping import EarlyStopping
+
+from dataset.preparetion import generate_data
+from dataset.travel_time_dataset import TravelTimeDataset
--- models/conv_lstm/model/convlstm.py
+++ models/conv_lstm/model/convlstm.py
@@ -1,113 +1,110 @@
-import torch
 import torch.nn as nn
+import torch
 
 class ConvLSTMBlock(nn.Module):
-    def __init__(self, input_channels, num_features, kernel_size=3, padding=1, stride=1):
+
+    def __init__(self, input_channels, hidden_channels, padding, kernel_size):
         super(ConvLSTMBlock, self).__init__()
-        self.num_features = num_features
-        self.conv = self._make_layer(input_channels + num_features, 4 * num_features, kernel_size, padding, stride)
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+        self.kernel_size = kernel_size
+       
+        self.padding = padding
+        self.num_features = 1
+        self.conv = nn.Conv1d(in_channels = self.input_channels + self.hidden_channels,
+                              out_channels = self.hidden_channels*4, kernel_size = self.kernel_size,padding=padding)                    
 
-    def _make_layer(self, input_channels, output_channels, kernel_size, padding, stride):
-        return nn.Sequential(
-            nn.Conv2d(input_channels, output_channels, kernel_size=kernel_size, padding=padding, stride=stride, bias=False),
-            nn.BatchNorm2d(output_channels)
+
+    def forward(self, input, h_cur ,c_cur):
+        #print(input.shape)
+        #print(h_cur.shape)
+        combined = torch.cat([input, h_cur], dim=1)  # concatenate along channel axis
+        #print(combined.shape)
+        #combined = combined.reshape(combined.shape[0],combined.shape[1],combined.shape[2],1)
+        #print(combined.shape)
+
+        combined_conv = self.conv(combined)
+        #print(combined_conv.shape)
+        # (cc_i, cc_f, cc_o, cc_g) = torch.split(combined_conv, int(combined_conv.size()[1] / self.num_features), dim=1)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_channels, dim=1)
+        i = torch.sigmoid(cc_i)
+        #print(i.shape)
+        f = torch.sigmoid(cc_f)
+        #print(f.shape)
+        o = torch.sigmoid(cc_o)
+       # print(o.shape)
+        g = torch.tanh(cc_g)
+        #print(g.shape)
+        test = f * c_cur
+        test2 = i* g
+        c_next = (f * c_cur) + (i * g)
+        #print(c_next)
+        h_next = o * torch.tanh(c_next)
+
+        return h_next, c_next
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class ConvLSTM(nn.Module):
+    
+    def __init__(self, input_channels,hidden_channels, padding,kernel_size ):
+        super(ConvLSTM, self).__init__()
+        self.hidden_channels = hidden_channels
+        self.conv = ConvLSTMBlock(input_channels,hidden_channels,padding,kernel_size)
+
+    def forward(self, input): #input = frame sequnce (batch_size, num_channels, seq_len, length)
+        #print(input.shape)
+        input = input.reshape(input.shape[0],input.shape[1],input.shape[2],1)
+        batch_size, _, seq_len, length = input.size() #get dimension
+        output = torch.zeros(batch_size,self.hidden_channels,seq_len,length,device = device) #Initial output
+        H = torch.zeros(batch_size,self.hidden_channels,length,device=device) #Initial hidden state
+        C = torch.zeros(batch_size,self.hidden_channels,length,device=device) #Initial cell input
+        
+        for time_step in range (seq_len):
+            H,C = self.conv(input[:,:,time_step],H,C)
+            #print(H.shape)
+            #print(C.shape)
+            output[:,:,time_step] = H
+        
+        output = output.reshape(output.shape[0],output.shape[1],output.shape[2])
+
+        return output 
+
+class ConvLSTMTravelTime(nn.Module):
+
+    def __init__(self,num_channels,num_kernels,kernel_size,padding,num_layers):
+        super(ConvLSTMTravelTime, self).__init__()
+
+        self.sequential = nn.Sequential()
+        
+        #ADD First Layer
+        self.sequential.add_module(
+            "convlstm1",ConvLSTM(input_channels=num_channels, hidden_channels=num_kernels,kernel_size=kernel_size,padding=padding)
+        )    
+        self.sequential.add_module(
+            "batchnorm1", nn.BatchNorm1d(num_features=num_kernels) 
         )
 
-    def forward(self, inputs):
-        outputs = []
-        B, S, C, H, W = inputs.shape
-        hx = torch.zeros(B, self.num_features, H, W).to(inputs.device)
-        cx = torch.zeros(B, self.num_features, H, W).to(inputs.device)
+        #Add other Layer
+        for i in range(2,num_layers+1):
+            self.sequential.add_module(
+                f"convlstm{i}",ConvLSTM(input_channels=num_kernels, hidden_channels=num_kernels,kernel_size=kernel_size,padding=padding)
+            )
+            self.sequential.add_module(
+                f"batchnorm{i}",nn.BatchNorm1d(num_features=num_kernels)
+            )   
+        
+        # Add Convolutional Layer to predict output 
+        #self.conv = nn.Conv1d(in_channels=num_kernels, out_channels=num_channels,kernel_size=kernel_size,padding=1 )
 
-        for t in range(S):
-            combine = torch.cat([input[:, t], hx], dim=1)
-            gates = self.conv(combine)
+        #add liner Layer to predict output
+        self.linear = nn.Linear(in_features=num_kernels,out_features=1)
+        
+    def forward(self,input):
+        output = self.sequential(input)
+        output = self.linear(output[:,:,-1])
 
-            ingate, forgetgate, cellgate, outgate = torch.split(gates, self.num_features, dim=1)
-            ingate = torch.sigmoid(ingate)
-            forgetgate = torch.sigmoid(forgetgate)
-            outgate = torch.sigmoid(outgate)
-            cellgate = torch.tanh(cellgate)
+        return nn.Sigmoid()(output)
 
-            cy = (forgetgate * cx) + (ingate * cellgate)
-            hy = outgate * torch.tanh(cy)
 
-            outputs.append(hy)
-            hx = hy
-            cx = cy
-
-        return torch.stack(outputs).permute(1, 0, 2, 3, 4).contiguous()
-
-class Encoder(nn.Module):
-    def __init__(self, cfg):
-        super(Encoder, self).__init__()
-        self.convlstm_1 = ConvLSTMBlock(
-            input_channels=cfg['encoder']['input_channels'][0], 
-            num_features=cfg['encoder']['num_features'][0], 
-            kernel_size=cfg['encoder']['kernel_size'][0], 
-            padding=cfg['encoder']['padding'][0], 
-            stride=cfg['encoder']['stride'][0]
-        )
-        self.convlstm_2 = ConvLSTMBlock(
-            input_channels=cfg['encoder']['input_channels'][1], 
-            num_features=cfg['encoder']['num_features'][1], 
-            kernel_size=cfg['encoder']['kernel_size'][1], 
-            padding=cfg['encoder']['padding'][1], 
-            stride=cfg['encoder']['stride'][1]
-        )
-
-    def forward(self, x):
-        out = [x]
-        x = self.convlstm_1(x)
-        out.append(x)
-        x = self.convlstm_2(x)
-        out.append(x)
-
-        return out
-
-class Decoder(nn.Module):
-    def __init__(self, cfg):
-        super(Decoder, self).__init__()
-        self.convlstm_1 = ConvLSTMBlock(
-            input_channels=cfg['decoder']['input_channels'][0],
-            num_features=cfg['decoder']['num_features'][0],
-            kernel_size=cfg['decoder']['kernel_size'][0],
-            padding=cfg['decoder']['padding'][0],
-            stride=cfg['decoder']['stride'][0]
-        )
-        self.convlstm_2 = ConvLSTMBlock(
-            input_channels=cfg['decoder']['input_channels'][1],
-            num_features=cfg['decoder']['num_features'][1],
-            kernel_size=cfg['decoder']['kernel_size'][1],
-            padding=cfg['decoder']['padding'][1],
-            stride=cfg['decoder']['stride'][1]
-        )
-
-    def forward(self, encoder_outputs):
-        idx = len(encoder_outputs) - 1
-
-        x = encoder_outputs[idx]
-
-        idx -= 1
-        x = torch.cat([encoder_outputs[idx], x], dim=2)
-        x = self.convlstm_1(x)
-        encoder_outputs[idx] = x
-
-        idx -= 1
-        x = torch.cat([encoder_outputs[idx], x], dim=2)
-        x = self.convlstm_2(x)
-        encoder_outputs[idx] = x
-
-        return x
-
-class TravelTimeModel(nn.Module):
-    def __init__(self, cfg):
-        super(TravelTimeModel, self).__init__()
-        self.encoder = Encoder(cfg)
-        self.decoder = Decoder(cfg)
-
-    def forward(self, x):
-        out = self.encoder(x)
-        out = self.decoder(out)
-
-        return out
+       
